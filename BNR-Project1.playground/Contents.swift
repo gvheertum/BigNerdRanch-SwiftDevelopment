8b280d7418c1fa91aa70dce0b916f417bc9d56f9
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-20 17:45:56 +0200

    First playground finalized and moved on to the next

--- BNR-Project1.playground/Contents.swift
+++ BNR-Project1.playground/Contents.swift
@@ -4,4 +4,26 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+//########################################################################
+// Part I - var, let, assignment and strings
+// Good to start with the basis (even if you know this in other languages
+// starting with this in new languages will give you a nice start)
+//########################################################################
+
+var str : String = "Hello, playground";
+str += "! So this is equal to 'normal' languages ;)";
+print(str);
+
+let fixedStr : String = "This string is not able to change!";
+//fixedStr += " So this is an error, no?" //Lol, this randomly killed my xcode instance after entering the semi-colon at the end of the line
+print(fixedStr);
+
+var editableNr : Int = 4;
+editableNr = 5;
+let nonEditableNr : Int = 10;
+//nonEditableNr = 11;
+
+print(editableNr, "-", nonEditableNr); //Note to self: the + operator does not work on Int & string
+var interpolatedString = "This is kinda neat, like the dot net version of {} we can use \\() for output: \(editableNr), \(nonEditableNr) (even calculated: (\(editableNr - nonEditableNr)))";
+print(interpolatedString)
+
