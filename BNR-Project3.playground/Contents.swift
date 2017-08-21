188a0fd9ca9153f0a158bf03bb34b16d013c6017
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-21 11:59:55 +0200

    Done whith loops

---
+++ BNR-Project3.playground/Contents.swift
@@ -0,0 +1,24 @@
+//Big nerd Ranch - Swift development - Playground 3
+
+import Cocoa
+//########################################################################
+// Part II - Basics of Swift
+// Loops
+//########################################################################
+
+
+//for (var i = 0; i < 10; i++) //Classic style if is removed in swift 3
+print("Starting");
+
+var cnt : Int = 0;
+/*for i in 1...5 {
+	cnt += 1; //the ++ is removed... :X
+	print("Counter: \(cnt)");
+}*/
+
+let lowerLimit : Int = 0;
+let upperLimit : Int = 10;
+/*for i in lowerLimit...upperLimit
+{
+	print("Counter: \(i)");
+}*/
