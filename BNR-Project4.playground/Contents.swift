cfc27490c460cacc8ae93ce41957345a6b308f29
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-21 12:12:45 +0200

    Created project 4 and started playing with strings

---
+++ BNR-Project4.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+//Big nerd Ranch - Swift development - Playground 2
+
+import Cocoa
+import Foundation
+//########################################################################
+// Part II - Basics of Swift
+// Playing with strings
+//########################################################################
+
+var myStr : String = "Hello world!";
+for char : Character in myStr.characters
+{
+	print("Found char: \(char)")
+}
