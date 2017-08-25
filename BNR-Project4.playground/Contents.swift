a49f0e4ff3d09f4ab8079a611a33c990011af84a
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-25 16:21:13 +0200

    conanical strings

--- BNR-Project4.playground/Contents.swift
+++ BNR-Project4.playground/Contents.swift
@@ -8,8 +8,38 @@ import Foundation
 // Playing with strings
 //########################################################################
 
-var myStr : String = "Hello world!";
+var myStr : String = "abc ABC Hello world! \u{1F60E}";
 for char : Character in myStr.characters
 {
 	print("Found char: \(char)")
 }
+
+for unicodeScalar in myStr.unicodeScalars
+{
+	print("Scalar: \(unicodeScalar) code: \(unicodeScalar.value)")
+}
+
+
+var accented : String = "\u{0061}\u{0301}"; //á
+var accented2 : String = "\u{00E1}"; //á
+var accented3: String = "á";
+for unicodeScalar in accented.unicodeScalars
+{
+	print("Scalar: \(unicodeScalar) code: \(unicodeScalar.value)")
+} // echoes 2 chars: 97 (a) & 769 (accent)
+
+for unicodeScalar in accented2.unicodeScalars
+{
+	print("Scalar: \(unicodeScalar) code: \(unicodeScalar.value)")
+} //Echoes a single char (255)
+
+for unicodeScalar in accented3.unicodeScalars
+{
+	print("Scalar: \(unicodeScalar) code: \(unicodeScalar.value)")
+} //Echoes a single char (255)
+
+let equal = accented == accented2 && accented2 == accented3;
+print("equality: \(equal)"); //Swift doesn't care about the method the accented char is created
+
+print("Counts a1: \(accented.characters.count), a2: \(accented2.characters.count), a3: \(accented3.characters.count)"); //Char count is 1 in all cases (which makes sense since the combined scalar really is only one char
+
