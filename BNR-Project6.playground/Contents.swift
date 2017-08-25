2f3aa706bebf0cb252052aeb4c76d03ba2b3582c
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-25 17:22:04 +0200

    Created playground for Part III

---
+++ BNR-Project6.playground/Contents.swift
@@ -0,0 +1,10 @@
+//Big nerd Ranch - Swift development - Playground 6
+
+
+import Cocoa
+
+//########################################################################
+// Part III - Collections and functions
+// Great DRY and more complexity compared to strings and integers
+//########################################################################
+
