8c4d03c87d5d17cd9be4e17a998bf28a560c2e97
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-26 16:53:16 +0200

    Done with the dictionaries

--- BNR-Project7.playground/Contents.swift
+++ BNR-Project7.playground/Contents.swift
@@ -26,8 +26,27 @@ print(myDict3);
 myDict.updateValue(-2, forKey: "Two") //Like the key usage, this will insert the item
 print(myDict);
 
-//You can remove th items by key (however the name is value, which is a bit odd)
+//You can remove items (values) by key
 myDict3.removeValue(forKey: "Two"); //like arrays this will return the object
 myDict3.removeValue(forKey: "Three"); //this returns nil
+//myDict3["One"] = nil; //You can also remove an item by setting it to nil, this will decrease the count, and remove the key. This however does NOT return any value (like the removeValue does, which is essentially a "pop")
 print(myDict3);
 print(myDict3);
+
+//Iterating through dictionaries
+for var (key, value) in myDict
+{
+	print("key \(key) -> value \(value)");
+}
+for var value in myDict3.values //Iterate the values
+{
+	print(value)
+}
+for var key in myDict3.keys //Iterate the keys
+{
+	print(key)
+}
+
+//The keys and values can all be extracted into arrays
+var keysArray : [String] = Array(myDict3.keys);
+var valuesArray : [Int] = Array(myDict3.values);
