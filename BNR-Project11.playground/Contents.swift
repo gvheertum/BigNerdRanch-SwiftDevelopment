7f58aa8d7eb5c65d134edbfaeb72ecea661be817
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-26 18:24:25 +0200

    Created project for Enumerations

---
+++ BNR-Project11.playground/Contents.swift
@@ -0,0 +1,11 @@
+//Big nerd Ranch - Swift development - Playground 11
+
+
+import Cocoa
+
+//########################################################################
+// Part IV - Enumerations, Structures and Classes
+// Finally something to store all these datas in ;)
+//########################################################################
+
+//In this playground: Enumerations
