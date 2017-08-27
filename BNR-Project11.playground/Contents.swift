af10c100cfa4a431f158808d29f7a2baecec1683
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-08-27 17:14:56 +0200

    Enums and functions

--- BNR-Project11.playground/Contents.swift
+++ BNR-Project11.playground/Contents.swift
@@ -105,3 +105,77 @@ print(ProgrammingLanguage.csharp.rawValue);
 
 var plCast1 : ProgrammingLanguage? = ProgrammingLanguage(rawValue: "c#");
 var plCast2 : ProgrammingLanguage? = ProgrammingLanguage(rawValue: "csharp"); //Mapping only happens on the string value, so the key is not used for construction (which make sense since a value can be both a key and a value for a different key)
+
+
+//enums can have functions, which can work on the value of the enum variable
+enum Status
+{
+	case on
+	case off
+	func echoYourStatus() -> String
+	{
+		switch self
+		{
+		case .on:
+			return "I'm on!";
+		case .off:
+			return "I'm off!";
+		}
+	}
+	mutating func toggle() -> Void //By default func is not able to mutate the instance, flagging as mutating will allow you to do this
+	{
+		switch self
+		{
+		case .on:
+			self = .off;
+		case .off:
+			self = .on;
+		}
+	}
+}
+var itemStatus : Status = Status.on;
+print(itemStatus.echoYourStatus());
+itemStatus = .off;
+print(itemStatus.echoYourStatus());
+
+//You can also toggle the status of the enum object itself
+itemStatus.toggle();
+print(itemStatus);
+itemStatus.toggle();
+print(itemStatus);
+
+let immutableStatus : Status = Status.on;
+//immutableStatus.toggle(); //This toggles a runtime error since the element is immutable but is calling a mutating function
+
+
+// Values in enums
+// There can be much more magic in enumerators
+enum Shape
+{
+	case point //you can have items without values mixed in these enums
+	case square(side: Double)
+	case rectange(side1: Double, side2: Double)
+	func getArea() -> Double
+	{
+		switch self
+		{
+		case .point:
+			return 0;
+		case let .rectange(side1: side1, side2: side2): //Note: use the let
+			return side1 * side2;
+		case let .square(side: side):
+			return side * side;
+		}
+	}
+}
+var sq = Shape.square(side: 10); //a "constructor is created
+var sq2 = Shape.square(side: 20);
+sq.getArea();
+//var sqEquality = sq == sq2; // Enums with values cannot be compared with the == (then again, comparing these squares would be interesting, are they equal because .square or not equal due to the value being different
+var rec = Shape.rectange(side1: 10, side2: 20);
+var rec2 = Shape.rectange(side1: 5, side2: 5);
+rec.getArea();
+
+var p = Shape.point;
+p.getArea();
+
