eabb7d077fc1c58dad978ab2312e1fa1445c140c
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-09-04 17:42:13 +0200

    Initial setup for garbage collection test

---
+++ GarbageTown/GarbageTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GarbageTown
+//
+//  Created by Gertjan on 04/09/2017.
+//  Copyright © 2017 Gertjan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
