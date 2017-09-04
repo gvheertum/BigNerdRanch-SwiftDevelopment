c215df15ae01b09e3adfcd15fba3a59e1c01fbb9
Gertjan van Heertum <gertjan@pulsatrix.nl>
2017-09-04 16:50:04 +0200

    typealiasses in protocols

--- BNR-Project16.playground/Contents.swift
+++ BNR-Project16.playground/Contents.swift
@@ -33,8 +33,11 @@ stack.pop();
 stack.pop();
 
 
+
 struct Stack<T>
 {
+	typealias Element = T;
+	
 	private	var items = [T]();
 	
 	mutating func push(_ newItem : T)
@@ -46,6 +49,34 @@ struct Stack<T>
 	{
 		guard !items.isEmpty else { return nil; }
 		return items.removeLast();
+	}
+	
+	func map<U>(_ f: (T) -> U) -> Stack<U>
+	{
+		var mapped = Stack<U>();
+		for item in items
+		{
+			mapped.push(f(item));
+		}
+		return mapped;
+	}
+	
+}
+
+protocol IteratorProtocol
+{
+	associatedtype Element;
+	mutating func next() -> Element?;
+}
+
+struct StackIterator<T> : IteratorProtocol
+{
+	typealias Element = T;
+	var stack: Stack<T>;
+	
+	mutating func next() -> Element?
+	{
+		return self.stack.pop();
 	}
 }
 
@@ -62,3 +93,36 @@ stringStack.push("TestString2");
 stringStack.pop();
 stringStack.pop();
 stringStack.pop();
+
+//Call mapping on a stack with numbers
+var mappingTestStack : Stack<Int> = Stack();
+mappingTestStack.push(1);
+mappingTestStack.push(3);
+mappingTestStack.push(5);
+var squaredTestStack = mappingTestStack.map({ $0 * $0});
+print(squaredTestStack);
+
+var tmpStackIterator = StackIterator(stack: squaredTestStack);
+while let itemInIterator = tmpStackIterator.next()
+{
+	print(itemInIterator);
+}
+
+//MAPPING FUNCTIONS (TEST)
+//Take array of T (items) convert T to U with F, yields array of U items
+func myMapper<T, U>(_ items: [T], _ f : (T) -> (U)) -> [U]
+{
+	var uRes = [U]();
+	for i in items
+	{
+		uRes.append(f(i));
+	}
+	return uRes;
+}
+
+func intToStr(_ i : Int) -> String
+{
+	return "\(i)";
+}
+var mappingRes = myMapper([1,2,3], intToStr);
+
